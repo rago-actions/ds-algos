1b19991239f28323bff4bc99425ccde78a2d7edc
Ramesh Godishela <49201509+rago-actions@users.noreply.github.com>
2019-07-15 02:27:21 +0530

    Create zip.py

---
+++ zip.py
@@ -0,0 +1,12 @@
+def zip(*iterables):
+    # zip('ABCD', 'xy') --> Ax By
+    sentinel = object()
+    iterators = [iter(it) for it in iterables]
+    while iterators:
+        result = []
+        for it in iterators:
+            elem = next(it, sentinel)
+            if elem is sentinel:
+                return
+            result.append(elem)
+        yield tuple(result)
