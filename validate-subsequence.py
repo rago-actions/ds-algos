45e5c9e9b9b5d2d4b1b578fbb458c08d2e0554c8
Ramesh Godishela <49201509+rago-actions@users.noreply.github.com>
2020-12-19 15:33:22 +0530

    Create validate-subsequence.py

---
+++ validate-subsequence.py
@@ -0,0 +1,18 @@
+def validateSubsequence(array, sequence):
+  arrIdx = 0
+  seqIdx = 0
+  while arrIdx < len(array) and seqIdx < len(sequence):
+      if array[arrIdx] == sequence[seqIdx]
+           seqIdx += 1
+      arrIdx += 1
+  return seqIdx == len(sequence)
+  
+  
+  def validateSubsequence(array, sequence):
+  seqIdx = 0
+  for value in array:
+      if seqIdx == len(sequence):
+         break // return True
+      if sequence[seqIdx] == values=
+           seqIdx += 1
+  return seqIdx == len(sequence)
